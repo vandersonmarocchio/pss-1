5efc33c67c4dd1fa3c2d623e1acc19ce8f864357
Douglas Mezuraro <douglasmez@gmail.com>
2018-05-06 04:19:14 -0300

    Adicionar arquivos de projeto.

---
+++ SGCO/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGCO.Models
+{
+    public class Produto
+    {
+        public Int32 Codigo { get; set; }
+        public String Descricao { get; set; }
+        public Double Valor { get; set; }
+        public String tags { get; set; }
+
+    }
+}
